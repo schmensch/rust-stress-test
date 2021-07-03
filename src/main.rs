2edae3ccb9f74599819dc7f679b795fef68645a0
Jonathan Zeller <jonathan_zeller@outlook.com>
2021-07-03 14:04:59 +0200

    main thread becomes worker instead of waiting

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use std::thread;
-use std::time::Duration;
 
 fn main() {
     let threads = num_cpus::get();
     println!("Threads in this system: {}", threads);
     println!("Using {} threads for stress test", threads);
-    for i in 0..threads {
+    for i in 1..threads {
         println!("Spawning thread number {}", i);
         thread::spawn (|| {
             let mut x = 0;
@@ -15,8 +14,9 @@ fn main() {
             }
         });
     }
-
+    let mut x = 0;
     loop {
-        Duration::from_millis(1000);
+        x = x + 1;
+        x = x - 1;
     }
 }
