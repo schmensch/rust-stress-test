a885d965babe53e528bf53cd837d4401406d80eb
Jonathan Zeller <jonathan_zeller@outlook.com>
2021-07-21 23:20:49 +0200

    Threads are now in functions, made thread counts human-readable

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use std::thread;
+use std::time::Duration;
 
 fn main() {
     let num_threads = num_cpus::get();
-    println!("Threads in this system: {}", num_threads);
-    println!("Using {} threads for stress test", num_threads);
+    println!("Threads in this system: {}", num_threads + 1);
+    println!("Using {} threads for stress test", num_threads + 1);
     for i in 0..num_threads {
         println!("Spawning thread number {}", i);
         worker();
+    }
+    loop {
+        thread::sleep(Duration::from_millis(1))
     }
 }
 
