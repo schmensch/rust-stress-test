6f619d8b0ac766f987071d4db519d40907e8d4b2
Jonathan Zeller <jonathan_zeller@outlook.com>
2021-07-21 22:51:18 +0200

    exported workers to external function, not final yet (doesn't run)

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,21 @@
 use std::thread;
 
 fn main() {
-    let threads = num_cpus::get();
-    println!("Threads in this system: {}", threads);
-    println!("Using {} threads for stress test", threads);
-    for i in 1..threads {
+    let num_threads = num_cpus::get();
+    println!("Threads in this system: {}", num_threads);
+    println!("Using {} threads for stress test", num_threads);
+    for i in 0..num_threads {
         println!("Spawning thread number {}", i);
-        thread::spawn (|| {
-            let mut x = 0;
-            loop {
-                x = x + 1;
-                x = x - 1;
-            }
-        });
+        worker();
     }
-    let mut x = 0;
-    loop {
-        x = x + 1;
-        x = x - 1;
-    }
+}
+
+fn worker() {
+    thread::spawn (|| {
+        let mut _x = 0;
+        loop {
+            _x += 1;
+            _x -= 1;
+        }
+    });
 }
