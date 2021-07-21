43582deacc28fe2fa94d806dfbf9cab43da58b64
Jonathan Zeller <jonathan_zeller@outlook.com>
2021-07-22 00:16:40 +0200

    Main thread is once again used as a worker

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,23 @@
 use std::thread;
-use std::time::Duration;
 
 fn main() {
     let num_threads = num_cpus::get();
     println!("Threads in this system: {}", num_threads + 1);
     println!("Using {} threads for stress test", num_threads + 1);
-    for i in 0..num_threads {
+    for i in 1..num_threads {
         println!("Spawning thread number {}", i);
-        worker();
+        thread::spawn (|| {
+            worker();
+        });
     }
-    loop {
-        thread::sleep(Duration::from_millis(1))
-    }
+    println!("Using main as last thread");
+    worker();
 }
 
 fn worker() {
-    thread::spawn (|| {
-        let mut _x = 0;
-        loop {
-            _x += 1;
-            _x -= 1;
-        }
-    });
+    let mut _x = 0;
+    loop {
+        _x += 1;
+        _x -= 1;
+    }
 }
